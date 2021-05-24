27ba7384c91d584851548ead48158d61ae9c53da
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-05-24 09:54:52 +0530

    feat: Migrated the demo-app and created a simple demo here

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,88 +1,64 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import ShapeDetailsBox from "./ShapeDetailsBox.js";
+import Shape from "react-clip-path";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-import Shape  from 'react-clip-path';
-import { getAvailableShapeTypes, getShape } from 'react-clip-path/schema';
-import { ShapeCards, ShapeCard, ShapeName, Playground, ShapePallete } from "./StyledComponents.js";
+import "./App.css";
 
 function App() {
-
-  const makeShapeObj = shapes => {
-    const availableShapes = getAvailableShapeTypes();
-    let obj = {};
-    availableShapes.forEach((elem) => {
-      let selected = elem === 'circle' ? true : false;
-      obj[elem] = selected;
-    });
-    console.log(obj);
-    return obj;
-  }
-
-  
-  const [selectedShape, setSelectedShape] = useState(getShape('circle'));
-  const [showShadow, setShowShadow] = useState(false);
-  const [shapeObj, setShapeObj] = useState(makeShapeObj());
-
-  const handleClick = (event, type) => {
-    const shapeInfo = getShape(type);
-    setShapeObj({...shapeObj, [type]: true, [selectedShape.type]: false});
-    setSelectedShape({...shapeInfo});
-    console.log(selectedShape);
-  }
-
-  const handleSwicth = event => {
-    setShowShadow(event.target.checked);
-  }
-
   return (
-    <Container fluid>
-      <Row>
-        <Col sm={4}>
-          <Playground>
-            <Shape
-              type={selectedShape.type}
-              width="300px"
-              height="300px"
-              showLabel={true}
-              showShadow={showShadow}
-            />
-            <Form style={{padding: '7px', textAlign: 'center'}}>
-              <Form.Check
-                type="switch"
-                id="custom-switch"
-                label="Show Outside of the Clipped Area"
-                onChange={handleSwicth}
-              />
-            </Form>
-            <ShapeDetailsBox shapeInformation={ selectedShape } />
-          </Playground>
-        </Col>
-        <Col sm={8}>
-          <ShapePallete>
-            <h2>Available Shapes({Reflect.ownKeys(shapeObj).length})</h2>
-            <ShapeCards>
-              {Reflect.ownKeys(shapeObj).map((shapeType, index) => (
-                <ShapeCard 
-                  key={index} 
-                  onClick={(e) => handleClick(e, shapeType)}
-                  selected = {shapeObj[shapeType]}
-                  >
-                  <Shape type={shapeType}/>
-                  <ShapeName>{getShape(shapeType).name}</ShapeName>
-                </ShapeCard>
-              ))}
-            </ShapeCards>
-          </ShapePallete>
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <Shape
+        type="circle"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={false}
+      />
+
+      <Shape
+        type="square"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={true}
+        backgroundColor="pink"
+      />
+
+      <Shape
+        type="rhombus"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={false}
+        backgroundColor="green"
+      />
+
+      <Shape
+        type="star"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={true}
+        backgroundColor="#c20b0b"
+      />
+
+      <Shape
+        type="cross"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={false}
+        backgroundColor="#da0983"
+      />
+
+      <Shape
+        text="Custom"
+        width="300px"
+        height="300px"
+        showLabel={true}
+        showShadow={false}
+        backgroundColor="#09ccda"
+        formula="polygon(10% 10%, 90% 10%, 90% 90%, 10% 80%)"
+      />
+    </div>
   );
 }
 
